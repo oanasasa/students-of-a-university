ed7e05ef2fd84a505cfee663aca772828058a87d
Oana <oana.sasaran@yahoo.com>
2023-06-21 23:11:02 +0300

    Controller and Routes setup

--- src/pages/home.component.jsx
+++ src/pages/home.component.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/layout.header";
 import "../App.css";
-import Student from "../components/student.component";
+// import Student from "../components/student.component";
 
 export default function HomePage() {
   // function createNewStudent() {
@@ -11,24 +11,75 @@ export default function HomePage() {
     <>
       <Header />
       <div id="homePage">
-        <h1>See all users:</h1>
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Degree</th>
-            <th>Specialization</th>
-            <th>Age of studing</th>
-            <th>Score</th>
-          </tr>
-          <tr>
-            {" "}
-            <Student />
-          </tr>
-          <tr>
-            <Student />
-          </tr>
-        </table>
+        <div class="container">
+          <h2>
+            Responsive Tables Using LI <small>Triggers on 767px</small>
+          </h2>
+          <ul class="responsive-table">
+            <li class="table-header">
+              <div class="col col-1">Job Id</div>
+              <div class="col col-2">Customer Name</div>
+              <div class="col col-3">Amount Due</div>
+              <div class="col col-4">Payment Status</div>
+            </li>
+            <li class="table-row">
+              <div class="col col-1" data-label="Job Id">
+                42235
+              </div>
+              <div class="col col-2" data-label="Customer Name">
+                John Doe
+              </div>
+              <div class="col col-3" data-label="Amount">
+                $350
+              </div>
+              <div class="col col-4" data-label="Payment Status">
+                Pending
+              </div>
+            </li>
+            <li class="table-row">
+              <div class="col col-1" data-label="Job Id">
+                42442
+              </div>
+              <div class="col col-2" data-label="Customer Name">
+                Jennifer Smith
+              </div>
+              <div class="col col-3" data-label="Amount">
+                $220
+              </div>
+              <div class="col col-4" data-label="Payment Status">
+                Pending
+              </div>
+            </li>
+            <li class="table-row">
+              <div class="col col-1" data-label="Job Id">
+                42257
+              </div>
+              <div class="col col-2" data-label="Customer Name">
+                John Smith
+              </div>
+              <div class="col col-3" data-label="Amount">
+                $341
+              </div>
+              <div class="col col-4" data-label="Payment Status">
+                Pending
+              </div>
+            </li>
+            <li class="table-row">
+              <div class="col col-1" data-label="Job Id">
+                42311
+              </div>
+              <div class="col col-2" data-label="Customer Name">
+                John Carpenter
+              </div>
+              <div class="col col-3" data-label="Amount">
+                $115
+              </div>
+              <div class="col col-4" data-label="Payment Status">
+                Pending
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
